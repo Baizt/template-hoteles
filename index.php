45d931ee58b31eb0319618257982240b1d91eccb
Al Baizt <albaizt@gmail.com>
2020-02-17 00:27:37 -0600

    Seleccion de templates

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Selecciona tu template</title>
+        <link rel="stylesheet" href="css/main.css">
+    </head>
+    <body>
+        <h1>Selecciona un Demo para Visualizarlo</h1>
+
+        <div class="opciones_container">
+            <a href="index_1.php" target="_blank" rel="next" id="opcion_1" class="opcion">
+                <h2>Opción 1</h2>
+                <div class="image-container">
+                    <img src="sources/template_1.jpeg" alt="Primer template">
+                </div>
+                <div class="ver_demo"> Ver Demo >></div>
+            </a>
+
+            <a href="index_2.php" target="_blank" rel="next" id="opcion_2" class="opcion">
+                <h2>Opción 2</h2>
+                <div class="image-container">
+                    <img src="sources/template_1.jpeg" alt="Segundo template">
+                </div>
+                <div class="ver_demo"> Ver Demo >></div>
+            </a>
+
+            <a href="#" rel="next" target="_blank" id="opcion_3" class="opcion">
+                <h2>Opción 3</h2>
+                <div class="image-container">
+                    <img src="sources/template_1.jpeg" alt="Template template">
+                </div>
+                <div class="ver_demo"> Ver Demo >></div>
+            </a>
+        </div>
+    </body>
+</html>
